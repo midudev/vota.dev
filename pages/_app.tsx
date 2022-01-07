4c59429dabe5fbe1166c4a20ea000e873f6d31bb
David Ponce Vargas <buap.david@gmail.com>
2022-01-06 22:32:19 -0600

    chore: remove useless styles and use relative import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
-import '../styles/globals.css'
+import 'styles/globals.css'
 
 function MyApp ({ Component: App, pageProps: { session, ...pageProps } }: AppProps) {
   return (
