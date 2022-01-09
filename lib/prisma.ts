e2ac0471b1bca47d621be86cf7c61b2d3caa19af
Miguel Ángel Durán <miduga@gmail.com>
2022-01-09 09:33:32 +0000

    Fix global variable problems with prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,9 @@
 import { PrismaClient } from '@prisma/client'
 
+declare global {
+  var prisma: PrismaClient | undefined
+}
+
 // create only one prisma client instance for development
 const prisma = global.prisma || new PrismaClient()
 
