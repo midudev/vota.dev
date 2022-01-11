9dbd27658fda3a6efd08590df92b158a8ddcec68
Luis Gabriel Janco <88288135+LuiSauter@users.noreply.github.com>
2022-01-11 10:44:50 -0400

    Fix NavUserOutlet
    fix the logout to not reload the page

--- components/NavUserOutlet.tsx
+++ components/NavUserOutlet.tsx
@@ -2,17 +2,24 @@
 import { LoadingIcon } from "components/LoadingIcon"
 import { LoginButton } from "components/LoginButton"
 import { signOut, useSession } from "next-auth/react"
+import { useRouter } from 'next/router'
 
 export function NavUserOutlet() {
   const { data: session, status } = useSession()
+  const router = useRouter()
 
   if (status === "loading") return <LoadingIcon />
   if (status === "unauthenticated" || session == null) return <LoginButton />
+  
+  const handleSignOut = async () => {
+    const data = await signOut({ redirect: false })
+    return router.push(data.url)
+  }
 
   return (
     <>
       Signed in as {session.user?.email} <br />
-      <button onClick={() => signOut({ redirect: false })}>Sign out</button>
+      <button onClick={handleSignOut}>Sign out</button>
     </>
   )
 }
