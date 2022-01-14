397647c02fa53ee36ed1595b87a319efd69438bb
Miguel Ángel Durán <miduga@gmail.com>
2022-01-14 12:37:28 +0100

    Add new logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,10 @@
 import { NavUserOutlet } from "components/NavUserOutlet"
+import { Logo } from "./Logo"
 
 export function Header() {
   return (
-    <header className='flex items-center justify-between w-full max-w-4xl mx-auto my-4'>
-      <h1 className='text-xl font-bold'>🗳 vota.dev</h1>
+    <header className='flex items-center justify-between w-full max-w-4xl px-2 mx-auto my-4'>
+      <Logo />
       <div>
         <NavUserOutlet />
       </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import '../styles/globals.css'
 function MyApp ({ Component: App, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <div className='flex flex-col min-h-screen bg-yellow-js align-center'>
+      <div className='flex flex-col min-h-screen px-2 bg-yellow-js align-center'>
         <App {...pageProps} />
       </div>
     </SessionProvider>
