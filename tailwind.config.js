299b1d34aa553c5c56e6110f19d2e0faa8ac830a
Miguel Ángel Durán <miduga@gmail.com>
2022-01-14 11:27:25 +0100

    Add custom color for Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'yellow-js': '#f7df1e',
+      }
+    }
   },
   plugins: []
 }
