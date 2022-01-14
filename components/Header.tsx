d5a6868af6ea92e13173ad60cd50a08d2dfbfa6f
Miguel Ángel Durán <miduga@gmail.com>
2022-01-14 11:28:06 +0100

    Improve Header styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,11 @@ import { NavUserOutlet } from "components/NavUserOutlet"
 
 export function Header() {
   return (
-    <header>
-      <NavUserOutlet />
+    <header className='flex items-center justify-between w-full max-w-4xl mx-auto my-4'>
+      <h1 className='text-xl font-bold'>🗳 vota.dev</h1>
+      <div>
+        <NavUserOutlet />
+      </div>
     </header>
   )
 }
