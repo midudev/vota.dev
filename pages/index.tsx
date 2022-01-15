6898b54077de5270e50d18d53ad11895d9be9a7a
Cristhofer Montalvo <cristhofermontalvo@gmail.com>
2022-01-15 13:58:23 -0500

    remove extra margin

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,11 +18,11 @@ interface BoxProps{
 }
 
 const Box = ({image, title, svg}: BoxProps) => <button className='pointer group'>
-  <div className='flex flex-row items-center gap-2 px-8 py-2 border border-gray-200 rounded-3xl '>
+  <div className='flex flex-row items-center gap-2 px-8 py-3 border border-gray-200 rounded-3xl '>
     <figure className='flex object-contain w-10 h-10'>
       {svg ? svg : image ? <img className='object-contain w-10 h-10' alt={title} src={image} /> : <SVG.Loading className="w-6 h-6 mr-1 animate-spin place-self-center"/>}
     </figure>
-    <div className='mt-4 text-2xl font-semibold text-gray-700'>
+    <div className='text-2xl font-semibold text-gray-700'>
       {title}
     </div>
   </div>
