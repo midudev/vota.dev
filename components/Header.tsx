3ed72de810d54f916bdb312f7a088de89b105ca5
Edgar Laz <edgarguitarist@gmail.com>
2022-01-15 19:14:25 -0500

    Changing and Fixing SVGs

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,9 @@ import { Logo } from "./Logo"
 export function Header() {
   return (
     <header className='flex items-center justify-between w-full max-w-4xl px-2 mx-auto my-4'>
-      <Logo />
+      <div className="inline-flex">
+      <Logo /> <h1 className="ml-5 text-2xl font-bold my-auto">vota.dev</h1>
+      </div>
       <div>
         <NavUserOutlet />
       </div>
