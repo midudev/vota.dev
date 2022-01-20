d2d046d1fd6f8f6a5d4d37d7a634b40a3a599184
Miguel Ángel Durán <miduga@gmail.com>
2022-01-20 20:38:27 +0100

    Fix linter error build

--- components/VotePill.tsx
+++ components/VotePill.tsx
@@ -1,6 +1,18 @@
 import { ReactElement } from 'react'
 import * as SVG from "components/SVGIcons"
 
+const VotePillImage = ({svg, title, image}: any) => {
+  if (svg) return svg
+  if (image)
+    return (
+      <img className="object-contain w-10 h-10" alt={title} src={image} />
+    )
+
+  return (
+    <SVG.Loading className="w-6 h-6 mr-1 animate-spin place-self-center" />
+  )
+}
+
 interface VotePillProps {
   image?: string,
   title?: string
@@ -8,25 +20,11 @@ interface VotePillProps {
 }
 
 export const VotePill = ({image, title, svg}: VotePillProps) => {
-  const getPictureElement = () => {
-    if (svg) return svg
-    if (image)
-      return (
-        <img className="object-contain w-10 h-10" alt={title} src={image} />
-      )
-
-    return (
-      <SVG.Loading className="w-6 h-6 mr-1 animate-spin place-self-center" />
-    )
-  }
-
-  const pictureElement = getPictureElement()
-
   return (
     <button className="pointer group">
       <div className="flex flex-row items-center gap-2 px-8 py-3 border border-gray-200 rounded-3xl ">
         <figure className="flex object-contain w-10 h-10">
-          {pictureElement}
+          <VotePillImage svg={svg} title={title} image={image} />
         </figure>
         <div className="text-2xl font-semibold text-gray-700">{title}</div>
       </div>
