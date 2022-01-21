26aa203328fc4ffce9b7a020f64956c2500ac43b
Miguel Ángel Durán <miduga@gmail.com>
2022-01-21 11:06:29 +0100

    Add config for i18n

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  i18n: {
+    locales: ['en', 'es'],
+    defaultLocale: 'es',
+  },
 }
