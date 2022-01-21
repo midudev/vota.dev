4ae8368bd3de1999232ac2b52da9677289e4132c
Edgar Laz <edgarguitarist@gmail.com>
2022-01-21 12:29:06 -0500

    Giving life to pills

--- components/VotePill.tsx
+++ components/VotePill.tsx
@@ -11,15 +11,13 @@ const VotePillImage = ({ svg, title, image }: VotePillProps) => {
   if (svg) return svg
   if (image)
     return <img className="object-contain w-10 h-10" alt={title} src={image} />
-
   return <SVG.Loading className="w-6 h-6 mr-1 animate-spin place-self-center" />
 }
-
 
 export const VotePill = ({ image, title, svg }: VotePillProps) => {
   return (
     <button className="pointer group">
-      <div className="flex flex-row items-center gap-2 px-8 py-3 border border-gray-200 rounded-3xl ">
+      <div className="flex flex-row items-center gap-2 px-8 py-3 border border-gray-200 rounded-3xl hover:scale-105 hover:border-midu-light">
         <figure className="flex object-contain w-10 h-10">
           <VotePillImage svg={svg} title={title} image={image} />
         </figure>
