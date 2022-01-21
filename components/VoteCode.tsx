b186430c35e95f2c7d65acf0c2956b1c30fd9448
nelsoncode019 <nelsonher019@gmail.com>
2022-01-21 12:50:39 -0600

    [FIX] Problems with Linter

--- components/VoteCode.tsx
+++ components/VoteCode.tsx
@@ -1,5 +1,5 @@
 import { ReactElement } from 'react'
-import * as SVG from 'components/SVGIcons'
+import Image from 'next/image'
 
 interface VotePillProps {
   image?: string
@@ -12,7 +12,11 @@ export const VoteCode = ({ image, title }: VotePillProps) => {
     <button className="pointer group">
       <div className="flex flex-col items-center">
         <figure className="flex object-contain w-full">
-          <img className="object-contain w-full" alt={title} src={image} />
+          <Image
+            className="object-contain w-full"
+            alt={title}
+            src={image ?? ''}
+          />
         </figure>
         <div className="text-2xl font-semibold text-gray-700">{title}</div>
       </div>
