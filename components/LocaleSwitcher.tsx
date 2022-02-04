2648b09678accba4fd5f9a60eccd4d7f630cfdd0
jpaddeo <jpaddeo@gmail.com>
2022-02-03 21:36:08 -0300

    Refactoring code. Adding bg yellow js.

--- components/LocaleSwitcher.tsx
+++ components/LocaleSwitcher.tsx
@@ -7,9 +7,8 @@ interface LocaleSwitcherProps {
 }
 
 function LocaleSwitcher({ useFlags = false }: LocaleSwitcherProps) {
-  const { locales, locale } = useRouter()
   const router = useRouter()
-  const { pathname, asPath, query } = router
+  const { locales, locale, pathname, asPath, query } = router
 
   const changeLocale = (nextLocale: string) => {
     router.push({ pathname, query }, asPath, { locale: nextLocale })
@@ -20,13 +19,13 @@ function LocaleSwitcher({ useFlags = false }: LocaleSwitcherProps) {
       <select
         defaultValue={locale}
         onChange={(ev) => changeLocale(ev.target.value)}
-        className="border-2 border-solid border-black text-black text-sm lg:text-xl p-1 uppercase bg-transparent font-semibold hover:opacity-80 transform transition duration-100 hover:scale-105"
+        className="border-2 border-solid border-black text-black text-sm lg:text-xl p-1 uppercase bg-transparent font-semibold hover:opacity-80 transform transition duration-100 hover:scale-105 rounded-md"
       >
         {locales?.map((locale) => (
           <option
             key={locale}
             value={locale}
-            className="flex items-center justify-center space-x-1 bg-transparent text-black font-bold"
+            className="flex items-center justify-center space-x-1 text-black font-bold bg-yellow-js"
           >
             {`${useFlags ? getFlagLocale(locale) : ''} ${locale}`}
           </option>
