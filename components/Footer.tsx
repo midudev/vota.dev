57e73cbc1bf888dfefc6399c84ab8e9f1d0e792a
Mateo Álvarez <alvarez.fing@gmail.com>
2022-02-18 12:32:59 -0300

    ref: change to new object of translations

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,7 +13,7 @@ export function Footer() {
           target="_blank"
           rel="noreferrer"
         >
-          {i18n.DEVELOPED_BY}
+          {i18n.FOOTER.DEVELOPED_BY}
           &nbsp;
           <picture className="flex flex-center rounded-full">
             <Image
@@ -59,7 +59,7 @@ export function Footer() {
           rel="nofollow noreferrer"
           target="_blank"
         >
-          {i18n.SUBMIT_SUGGESTION}
+          {i18n.FOOTER.SUBMIT_SUGGESTION}
         </a>
         <span>&bull;</span>
       </div>
